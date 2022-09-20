7fc74d8f7c78d79c53631ae36193057b29043019
Williamns Robles <robles.williamns@gmail.com>
2022-09-20 10:47:43 -0500

    Interfaces
    Interfaces

---
+++ interfaces.php
@@ -0,0 +1,24 @@
+<?php
+interface Automovil {
+    public function getTipo();
+    public function getRuedas();
+}
+
+class Coche implements Automovil {
+    public function getTipo(){
+        echo "Coche";
+    }
+    public function getRuedas(){
+        echo "4";
+    }
+}
+
+class Moto implements Automovil {
+    public function getTipo(){
+        echo "Moto";
+    }
+    public function getRuedas(){
+        echo "2";
+    }
+}
+?>
