79ccfe0a57e77f8b698da99ab843aa9303b5cb92
Williamns Robles <robles.williamns@gmail.com>
2022-09-20 16:37:05 -0500

    Interfaces
    Interfaces

--- interfaces.php
+++ interfaces.php
@@ -1,4 +1,13 @@
 <?php
+/*
+La interfaz es similar a las clases abstractas. La diferencia entre interfaces y clases abstractas son:
+
+Las interfaces no pueden tener propiedades, mientras que las clases abstractas pueden
+Todos los métodos de interfaz deben ser públicos, mientras que los métodos de clase abstracta son públicos o protegidos.
+Todos los métodos en una interfaz son abstractos, por lo que no se pueden implementar en el código y la palabra clave abstracta no es necesaria.
+Las clases pueden implementar una interfaz mientras heredan de otra clase al mismo tiempo
+*/
+
 interface Automovil {
     public function getTipo();
     public function getRuedas();
